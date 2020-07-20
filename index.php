ef3e105eed397f0c06d65ad7c91b0fa7bd4dff05
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-07-20 17:44:04 +0200

    Affiche les "cartes" dans un ordre aléatoire. Initialise l'état de la carte à 1 quand on clique sur son input

--- index.php
+++ index.php
@@ -1,56 +1,66 @@
 <?php 
-session_start();
 include 'class_carte.php'; 
-// session_destroy();
-    if(isset($_GET["choix"], $_GET["selecteur"]) && !isset($nb_cartes))
-        {            
-            $_SESSION["nb_paires"] = $_GET["selecteur"]; 
-            $_SESSION["nb_cartes"] = [];
-            $_SESSION["carte_retourne"] = [];
-            $_SESSION["carte_etat"] = [];           
+session_start();
 
-            var_dump($_SESSION["nb_cartes"]);
+if(isset($_GET["deco"]))
+    {
+        session_destroy();
+    }
+if(!isset($_SESSION["nb_cartes"]) && !isset( $_SESSION["carte_retourne"]) && !isset($_SESSION["carte_etat"]) && !isset($_SESSION["carte"]))
+    {
+        $_SESSION["nb_cartes"] = [];
+        $_SESSION["carte_retourne"] = [];
+        $_SESSION["carte_etat"] = [];        
+        $_SESSION["carte"]= [];
+    }
+
+
+// var_dump($_SESSION["nb_cartes"]);
+    if(isset($_GET["jeu"], $_GET["niveau"]) && empty($_SESSION["nb_cartes"]))
+        {                                  
+            $_SESSION["nb_paires"] = $_GET["niveau"]; 
+           
+           
             if(empty($_SESSION["nb_cartes"]) && empty($_SESSION["carte_etat"]))
-                {      
-                    echo'toto';                      
-                    for($i = 0; $i<$_SESSION["nb_paires"]; $i++)     
-                        {                    
-                            $carte = new Carte;                    
-                            array_push($_SESSION["nb_cartes"], $carte->getId());
-                            array_push($_SESSION["carte_etat"], $carte->getEtat());   
-                        }         
-                        var_dump($_SESSION["nb_cartes"]);
-                    shuffle($_SESSION["nb_cartes"]);   
-                    var_dump($_SESSION["nb_cartes"]);              
-                }   
-            else
-                echo 'pouet';
+                {                                        
+                    for($i = 1; $i<=$_SESSION["nb_paires"]; $i++)     
+                        {                                                                             
+                            array_push($_SESSION["nb_cartes"], $i, $i);                               
+                        }        
+                    for($i = 0; $i<($_SESSION["nb_paires"]*2); $i++) 
+                        {
+                            array_push($_SESSION["carte"], new Carte);
+                            array_push($_SESSION["carte_etat"], $_SESSION["carte"][$i]->getEtat());
+                        }
+                    
+                    shuffle($_SESSION["nb_cartes"]);                                 
+                }               
         }        
     
     if(isset($_SESSION["nb_paires"]))   
         {            
-            for($i = 0; $i<$_SESSION["nb_paires"]; $i++) 
-                {
+            for($i = 0; $i<($_SESSION["nb_paires"]*2); $i++) 
+                {                   
                     if($_SESSION["carte_etat"][$i] == 0 && !isset($_POST[$i]))
                         {
                             ?>
-                            <form action="" method="POST">
+                            <form action="index.php" method="POST">
                                 <input type="submit" name="<?= $i?>" id="">
                             </form>
                             <?php    
                         }   
                     if(isset($_POST[$i]))
                         {                            
-                            $_SESSION["carte_etat"][$i] = $carte->setEtat();    
+                            $_SESSION["carte_etat"][$i] = $_SESSION["carte"][$i]->setEtat();    
                             if($_SESSION["carte_etat"][$i] == 1)
                                 {                                    
                                     echo $_SESSION["nb_cartes"][$i];
                                     array_push($_SESSION["carte_retourne"], $i);
-                                }
+                                }                            
                         }      
-                }
-                // var_dump($_SESSION["nb_cartes"]);   
-        }                                                                                                                      
+                }                           
+        }            
+        var_dump($_SESSION);                                                                                                          
 ?>
 
 <!DOCTYPE html>
@@ -62,13 +72,14 @@ include 'class_carte.php';
 <body>
     <main>
         <form action="" method="GET">
-            <select name="selecteur" id="selecteur">    
-                <option value="8">4 paires</option>
-                <option value="12">6 paires</option>
-                <option value="16">8 paires</option>
-                <option value="20">10 paires</option>            
+            <select name="niveau" id="niveau">    
+                <option value="4">4 paires</option>
+                <option value="6">6 paires</option>
+                <option value="8">8 paires</option>
+                <option value="10">10 paires</option>            
             </select>
-            <input type="submit" name="choix" id="choix">
+            <input type="submit" name="jeu" id="jeu">
+            <input type="submit" name="deco" value="deco">
         </form>
     </main>
 </body>
